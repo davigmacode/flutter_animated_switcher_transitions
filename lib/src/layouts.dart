2b485b29941348aa127ce88921f4fab2b9455658
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-04-05 21:19:39 +0700

    Improved api doc

--- lib/src/layouts.dart
+++ lib/src/layouts.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/widgets.dart';
 
+/// This abstract class in Flutter provides a collection
+/// of pre-built layout builders for AnimatedSwitcher.
 abstract class AnimatedSwitcherLayouts {
-  /// The new child is placed in a [Stack] that sizes itself to match the
-  /// largest of the child or a previous child. The children are centered on each other.
-  static const defaults = AnimatedSwitcher.defaultLayoutBuilder;
+  /// This layout strategy creates a visually appealing transition
+  /// where the new widget appears on top of the outgoing widgets
+  /// while they fade out or animate out. The automatic resizing and centering
+  /// contribute to a seamless and polished animation experience
+  static const outIn = AnimatedSwitcher.defaultLayoutBuilder;
 
-  static const inverted = _inverted;
-  static Widget _inverted(Widget? currentChild, List<Widget> previousChildren) {
+  /// This stacking order allows the new child to take precedence visually
+  /// while the previous children fade out or animate out underneath.
+  /// The centered positioning creates a smooth transition without unexpected layout jumps.
+  static const inOut = _inOut;
+  static Widget _inOut(Widget? currentChild, List<Widget> previousChildren) {
     return Stack(
       alignment: Alignment.center,
       children: [
--- lib/src/transitions.dart
+++ lib/src/transitions.dart
@@ -1,91 +1,111 @@
 import 'package:flutter/widgets.dart';
 import 'dart:math';
 
+/// This abstract class in Flutter provides a collection
+/// of pre-built transition builders for AnimatedSwitcher.
+/// These transition builders define how widgets are animated
+/// when switching between them within the AnimatedSwitcher widget.
+/// They offer a variety of visual effects to enhance the user experience
+/// and make transitions between widgets more engaging.
 abstract class AnimatedSwitcherTransitions {
-  /// The new child is given a [FadeTransition] which increases opacity as
-  /// the animation goes from 0.0 to 1.0, and decreases when the animation is reversed.
+  /// Animates widgets by fading them in and out of view.
   static const fade = AnimatedSwitcher.defaultTransitionBuilder;
 
-  /// The new child is given a [ScaleTransition] which increases scale as
-  /// the animation goes from 0.0 to 1.0, and decreases when the animation is reversed.
+  /// Animates widgets by zooming them in.
   static const zoomIn = _zoomIn;
   static Widget _zoomIn(Widget child, Animation<double> animation) {
     return zoom(0, 1)(child, animation);
   }
 
-  /// The new child is given a [ScaleTransition] which decrease scale as
-  /// the animation goes from 2.0 to 1.0, and increase when the animation is reversed.
+  /// Animates widgets by zooming them out.
   static const zoomOut = _zoomOut;
   static Widget _zoomOut(Widget child, Animation<double> animation) {
     return zoom(2, 1, withFade: true)(child, animation);
   }
 
-  static const slideTop = _slideTop;
-  static Widget _slideTop(Widget child, Animation<double> animation) {
+  /// Animates widgets by sliding them out
+  /// from the right side relative to the widget.
+  static const slideRight = _slideRight;
+  static Widget _slideRight(Widget child, Animation<double> animation) {
     return slide(0)(child, animation);
   }
 
-  static const slideTopLeft = _slideTopLeft;
-  static Widget _slideTopLeft(Widget child, Animation<double> animation) {
-    return slide(-45)(child, animation);
-  }
-
-  static const slideTopRight = _slideTopRight;
-  static Widget _slideTopRight(Widget child, Animation<double> animation) {
-    return slide(45)(child, animation);
-  }
-
-  static const slideRight = _slideRight;
-  static Widget _slideRight(Widget child, Animation<double> animation) {
+  /// Animates widgets by sliding them out
+  /// from the bottom side relative to the widget.
+  static const slideBottom = _slideBottom;
+  static Widget _slideBottom(Widget child, Animation<double> animation) {
     return slide(90)(child, animation);
   }
 
-  static const slideBottom = _slideBottom;
-  static Widget _slideBottom(Widget child, Animation<double> animation) {
+  /// Animates widgets by sliding them out
+  /// from the left side relative to the widget.
+  static const slideLeft = _slideLeft;
+  static Widget _slideLeft(Widget child, Animation<double> animation) {
     return slide(180)(child, animation);
   }
 
-  static const slideBottomLeft = _slideBottomLeft;
-  static Widget _slideBottomLeft(Widget child, Animation<double> animation) {
+  /// Animates widgets by sliding them out
+  /// from the top side relative to the widget.
+  static const slideTop = _slideTop;
+  static Widget _slideTop(Widget child, Animation<double> animation) {
+    return slide(270)(child, animation);
+  }
+
+  /// Animates widgets by sliding them out
+  /// from the top left corner relative to the widget.
+  static const slideTopLeft = _slideTopLeft;
+  static Widget _slideTopLeft(Widget child, Animation<double> animation) {
     return slide(225)(child, animation);
   }
 
-  static const slideBottomRight = _slideBottomRight;
-  static Widget _slideBottomRight(Widget child, Animation<double> animation) {
+  /// Animates widgets by sliding them out
+  /// from the top right corner relative to the widget.
+  static const slideTopRight = _slideTopRight;
+  static Widget _slideTopRight(Widget child, Animation<double> animation) {
     return slide(135)(child, animation);
   }
 
-  static const slideLeft = _slideLeft;
-  static Widget _slideLeft(Widget child, Animation<double> animation) {
-    return slide(-90)(child, animation);
+  /// Animates widgets by sliding them out
+  /// from the bottom left corner relative to the widget.
+  static const slideBottomLeft = _slideBottomLeft;
+  static Widget _slideBottomLeft(Widget child, Animation<double> animation) {
+    return slide(315)(child, animation);
+  }
+
+  /// Animates widgets by sliding them out
+  /// from the bottom right corner relative to the widget.
+  static const slideBottomRight = _slideBottomRight;
+  static Widget _slideBottomRight(Widget child, Animation<double> animation) {
+    return slide(45)(child, animation);
   }
 
   /// Animates widgets by flipping them along the horizontal axis
-  /// (from left to right or vice versa)
+  /// (from left to right or vice versa).
   static const flipX = _flipX;
   static Widget _flipX(Widget child, Animation<double> animation) {
     return flip(Axis.horizontal)(child, animation);
   }
 
   /// Animates widgets by flipping them along the vertical axis
-  /// (from top to bottom or vice versa)
+  /// (from top to bottom or vice versa).
   static const flipY = _flipY;
   static Widget _flipY(Widget child, Animation<double> animation) {
     return flip(Axis.vertical)(child, animation);
   }
 
-  /// Animates widgets by rotating them around a central point
+  /// Creates a transition builder for AnimatedSwitcher that
+  /// animates widgets by rotating them around a central point.
   ///
-  /// [scaleFactor] (optional): A `double` value that controls the scaling
+  /// **[scaleFactor]** (optional): A `double` value that controls the scaling
   /// of the widgets during the rotation. Defaults to `1.0`, which means no scaling is applied.
   /// Values greater than `1.0` will make the widgets grow larger during the animation,
   /// while values less than `1.0` will make them shrink.
   ///
-  /// [clockwise] (optional): A `bool` value that determines the direction of rotation.
+  /// **[clockwise]** (optional): A `bool` value that determines the direction of rotation.
   /// Defaults to true, which means the widgets will rotate clockwise.
   /// Set it to false for counter-clockwise rotation.
   ///
-  /// [withFade] (optional): A `bool` value that controls whether the widgets
+  /// **[withFade]** (optional): A `bool` value that controls whether the widgets
   /// fade in and out during the transition. Defaults to `true`, which creates
   /// a fading effect along with the rotation. Set it to `false` to disable fading.
   static AnimatedSwitcherTransitionBuilder rotate({
@@ -115,19 +135,20 @@ abstract class AnimatedSwitcherTransitions {
     };
   }
 
-  /// Animates the appearance and disappearance of widgets using a zooming effect.
+  /// Creates a transition builder for AnimatedSwitcher that
+  /// animates the appearance and disappearance of widgets using a zooming effect.
   ///
-  /// [scaleInFactor] (required): A `double` value that determines
+  /// **[scaleInFactor]** (required): A `double` value that determines
   /// the scale factor for the incoming widget. A value greater than `1` (e.g., `1.2`)
   /// will make the incoming widget appear larger during the animation,
   /// while a value less than `1` (e.g., `0.8`) will make it appear smaller.
   ///
-  /// [scaleOutFactor] (required): A `double` value that determines
+  /// **[scaleOutFactor]** (required): A `double` value that determines
   /// the scale factor for the outgoing widget. A value greater than `1`
   /// will make the outgoing widget appear larger before disappearing,
   /// while a value less than 1 will make it appear smaller.
   ///
-  /// [withFade] (optional): A `bool` value that controls whether to include
+  /// **[withFade]** (optional): A `bool` value that controls whether to include
   /// a fading effect along with the zooming animation. Defaults to `false`.
   /// Setting it to `true` will cause the outgoing widget to fade out while being scaled,
   /// creating a more seamless transition.
@@ -155,20 +176,30 @@ abstract class AnimatedSwitcherTransitions {
     };
   }
 
-  /// Animates the incoming widget by sliding it
+  /// Creates a transition builder for AnimatedSwitcher that
+  /// animates the incoming widget by sliding it
   /// in to a specified direction and distance,
   /// also control additional effects like scaling and fading.
   ///
-  /// [distance] (optional): A `double` value that controls the distance
+  /// **[direction]** (required): A `double` value representing
+  /// the sliding direction in degrees, measured clockwise from
+  /// the positive x-axis (right). Here's the breakdown:
+  /// - `0.0`: Slide from right to left (default behavior).
+  /// - `90.0`: Slide from bottom to top.
+  /// - `180.0`: Slide from left to right.
+  /// - `270.0`: Slide from top to bottom.
+  /// - Values between these angles will result in diagonal slides.
+  ///
+  /// **[distance]** (optional): A `double` value that controls the distance
   /// the widget slides relative to its original position. Defaults to `1.0`,
   /// which means the widget slides its own width or height
   /// (depending on the slide direction). Larger values will result in a longer slide.
   ///
-  /// [scaleFactor] (optional): A `double` value that controls the scaling of the widgets
+  /// **[scaleFactor]** (optional): A `double` value that controls the scaling of the widgets
   /// during the animation. Defaults to `1.0` (no scaling). Values less than `1.0` will shrink
   /// the widgets, while values greater than `1.0` will enlarge them.
   ///
-  /// [withFade] (optional): A `bool` value that determines whether to include
+  /// **[withFade]** (optional): A `bool` value that determines whether to include
   /// a fading effect along with the slide animation. Defaults to `true`,
   /// which creates a smoother transition. Set it to `false` to disable fading.
   static AnimatedSwitcherTransitionBuilder slide(
@@ -179,7 +210,7 @@ abstract class AnimatedSwitcherTransitions {
   }) {
     return (child, final animation) {
       final isReversed = animation.status == AnimationStatus.reverse;
-      final directionRadians = getRadiansFromDegrees(direction - 90);
+      final directionRadians = getRadiansFromDegrees(direction);
       final offset = Offset.fromDirection(directionRadians, distance);
       final tween = Tween<Offset>(
         begin: isReversed ? -offset : offset,
@@ -201,26 +232,31 @@ abstract class AnimatedSwitcherTransitions {
     };
   }
 
-  /// Animates widgets by flipping them along a specified axis.
+  /// Creates a transition builder for AnimatedSwitcher that
+  /// animates widgets by flipping them along a specified axis.
+  ///
+  /// **[direction]** (optional): An Axis value that determines the flipping direction.
+  /// Defaults to Axis.horizontal, which flips widgets from left to right (or vice versa).
+  /// You can also use Axis.vertical to flip widgets from top to bottom (or vice versa).
   static AnimatedSwitcherTransitionBuilder flip([
     Axis direction = Axis.horizontal,
   ]) {
     return (child, final animation) {
-      final rotation = Tween(begin: pi, end: 0.0).animate(animation);
+      final flip = Tween(begin: pi, end: 0.0).animate(animation);
       return AnimatedBuilder(
-        animation: rotation,
+        animation: flip,
         child: child,
         builder: (context, child) {
           final isReversed = animation.status == AnimationStatus.reverse;
           double tilt = ((animation.value - 0.5).abs() - 0.5) * 0.003;
           tilt *= isReversed ? -1.0 : 1.0;
           final transform = Matrix4.identity()..setEntry(3, 0, tilt);
-          final value =
-              isReversed ? min(rotation.value, pi / 2) : rotation.value;
+          final tween = flip.value;
+          final angle = isReversed ? min(tween, pi / 2) : tween;
           if (direction == Axis.horizontal) {
-            transform.rotateY(value);
+            transform.rotateY(angle);
           } else {
-            transform.rotateX(value);
+            transform.rotateX(angle);
           }
           return Transform(
             transform: transform,
@@ -232,16 +268,40 @@ abstract class AnimatedSwitcherTransitions {
     };
   }
 
-  static AnimatedSwitcherTransitionBuilder shake() {
+  /// Creates a transition builder for AnimatedSwitcher that
+  /// animates a widget by shaking it horizontally or vertically.
+  ///
+  /// [direction] (optional): An `Axis` value that controls the shaking direction.
+  /// Defaults to `Axis.horizontal`, which shakes the widget back and forth horizontally.
+  /// You can also use `Axis.vertical` to shake the widget up and down vertically.
+  ///
+  /// [distance] (optional): A `double` value that determines
+  /// the maximum offset of the shaking movement. Defaults to `3.0`,
+  /// which creates a moderate shaking effect. Higher values will
+  /// result in more pronounced shaking.
+  ///
+  /// [withFade] (optional): A `bool` value that determines whether to fade
+  /// the widget during the shaking animation. Defaults to `false`,
+  /// meaning the widget will remain fully visible while shaking.
+  /// Set to `true` to fade the widget in and out along with the shaking motion.
+  static AnimatedSwitcherTransitionBuilder shake({
+    Axis direction = Axis.horizontal,
+    double distance = 3,
+    bool withFade = false,
+  }) {
     return (child, final animation) {
       final shake = Tween(begin: 0.0, end: 1.0).animate(animation);
       return AnimatedBuilder(
         animation: shake,
-        child: fade(child, animation),
+        child: withFade ? fade(child, animation) : child,
         builder: (context, child) {
-          final dx = sin(shake.value * pi * 5) * 5;
+          final isHorizontal = direction == Axis.horizontal;
+          final d = sin(shake.value * pi * distance) * distance;
           return Transform.translate(
-            offset: Offset(dx, 0.0),
+            offset: Offset(
+              isHorizontal ? d : 0.0,
+              isHorizontal ? 0 : d,
+            ),
             child: child,
           );
         },
